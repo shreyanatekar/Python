78b9283c428744aba6301707be2f18f88676d273
shreyanatekar <natekar.k.shreya@gmail.com>
2018-08-01 15:32:35 -0700

    change

--- Project1/test1.py
+++ Project1/test1.py
@@ -1,1 +1,9 @@
-Hellogkjj
+# A first Python script
+import sys # Load a library module
+#print(sys.platform)
+#print(2 ** 100) # Raise 2 to a power
+x = 'Spam!'
+#print(x * 8) # String repetition
+
+address = ["abc path", "apt 123", "New York"]
+pins =
